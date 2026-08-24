4447bc0c9ef54255401c53ed90d04751f12d860e
yuelingmingyou <yuelingmingyoudie@gmail.com>
2026-08-24 08:53:29 +0800

    Delete js directory

--- js/characters.js
+++
@@ -1,7 +0,0 @@
-// 额外角色加载 - 简化版
-function loadAdditionalCharacters() {
-    // 角色已在 data.js 中全部定义，此函数保留用于扩展
-    console.log('All characters loaded from data.js');
-}
-
-window.loadAdditionalCharacters = loadAdditionalCharacters;
